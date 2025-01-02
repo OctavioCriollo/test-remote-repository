403fdd0b3df0fddd0462596d8e863719485f1ef1
OctavioCriollo <122424073+OctavioCriollo@users.noreply.github.com>
2025-01-02 22:52:17 +1000

    new file git fetch origin

---
+++ git feth origin.sh
@@ -0,0 +1,3 @@
+git feth origin
+git checkout origin/“remote-branch” # conmuta a branch main del repositoria remote
+git checkout “local-branch”         # retorna a branch main del repositoria local
